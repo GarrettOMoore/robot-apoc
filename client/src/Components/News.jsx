c381a5d07ef18aecbfbaffc7f5c3da419e423552
mickeychcg <mcowanseattle@gmail.com>
2019-03-28 16:37:49 -0700

    added News and NewsDetail components

---
+++ client/src/Components/News.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const News = () => {
+  return (
+    <div ClassName="Breaking">
+      <h2> Breaking News!</h2>
+        <div ClassName="Headline">
+          <h3> Mass Attack in Brooklyn!</h3>
+        </div>
+     </div>
+  );
+
+}
+
+export default News;
---
+++ client/src/Components/NewsDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NewsDetail = (props) => {
+  return (
+    <div ClassName="Breaking">
+      <h2> Breaking News!</h2>
+        <div ClassName="Headline">
+          <h3> Mass Attack in Brooklyn!</h3>
+        </div>
+     </div>
+  );
+}
+
+export default NewsDetail;

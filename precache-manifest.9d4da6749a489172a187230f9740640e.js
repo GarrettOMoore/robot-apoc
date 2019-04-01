cb6d7b0a613f3279e666b7b1b86b97874c1a37ea
Garrett Moore <robertgarrettmoore@gmail.com>
2019-04-01 13:00:13 -0700

    Updates

---
+++ precache-manifest.9d4da6749a489172a187230f9740640e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "df6a589c444a6c7088abbc5ce602d4f8",
+    "url": "/static/media/StarbunkerTabBar.df6a589c.png"
+  },
+  {
+    "revision": "e97c036458ec5b918fd0479906eb24ab",
+    "url": "/static/media/Starbunker Logo.e97c0364.png"
+  },
+  {
+    "revision": "fd8defe9c3028a050821a07a65f21c7f",
+    "url": "/static/media/FrappyHourAd.fd8defe9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f5193242584b7a222b2",
+    "url": "/static/js/main.c06bce09.chunk.js"
+  },
+  {
+    "revision": "33d8fd713498abe35cbd",
+    "url": "/static/js/2.2e96e845.chunk.js"
+  },
+  {
+    "revision": "8f5193242584b7a222b2",
+    "url": "/static/css/main.0cfdadbf.chunk.css"
+  },
+  {
+    "revision": "33d8fd713498abe35cbd",
+    "url": "/static/css/2.1acc09b1.chunk.css"
+  },
+  {
+    "revision": "c1c5156797bfcd0917be94b093f4f706",
+    "url": "/index.html"
+  }
+];
